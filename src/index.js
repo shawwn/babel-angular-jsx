4d67b78b53f73519f1641794a3c430e308604949
Shawn Presser <shawnpresser@gmail.com>
2017-10-23 14:31:05 -0500

    Version 0.0.1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import jsx from "babel-plugin-syntax-jsx";
-const babel = require('babel-core')
+var jsx = require("babel-plugin-syntax-jsx");
+const babel = require('babel-core');
 import { convert } from 'angular-jsx';
 
 export default function ({ types: t }) {
     return {
         inherits: jsx,
-        visitor: {
-          Program: {
-            exit: function(path, state) {
-              if (path._done) return;
-              const { code: input } = babel.transformFromAst(state.file.ast);
-              const output = convert(input);
-              const { ast } = babel.transform(output);
-              path.replaceWith(ast.program);
-              path._done = true;
-            }
-          },
+        post(file) {
+          Object.assign(file, babel.transform(convert(file.code)))
         }
-    };
+    }
 }
